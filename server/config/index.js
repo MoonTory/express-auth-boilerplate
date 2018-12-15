bd19b69845f8d54c93f643aead5668450f6bde84
Gustavo Quinta <gustavoqnt40@gmail.com>
2018-12-14 23:48:03 -0200

    Update config

--- server/config/index.js
+++ server/config/index.js
@@ -4,10 +4,10 @@ export const {
   JWT_SECRET = 'superdupersectret',
 
 
-  DB_USER = 'moontory',
-  DB_PASS = 'tsuki951',
-  DB_HOST = 'ds143573.mlab.com',
-  DB_PORT = '43573',
-  DB_NAME = 'tsuki-dev',
+  DB_USER = '',
+  DB_PASS = '',
+  DB_HOST = '',
+  DB_PORT = '',
+  DB_NAME = '',
   MONGO_URI = `mongodb://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}`
 } = process.env
